d22a7b1108097e54f97eb53db0d242e4f0822907
ganeshka <gnkangne@gmail.com>
2026-02-18 23:47:35 +0530

    19th feb

--- src/main/java/org/selenium/SeleniumMainApp.java
+++ src/main/java/org/selenium/SeleniumMainApp.java
@@ -11,6 +11,7 @@ public class SeleniumMainApp {
         // Additional Selenium setup and code can be added here
 
         WebDriver driver= new ChromeDriver();
-        driver.get("https://app.vwo.com/");
+        driver.get("https://app.vwo.in/");
+
     }
 }
